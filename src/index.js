7eb04942f79ac1dc46f93dd0ef7b052bffd1e830
Taylor Masiello <masiellotay@gmail.com>
2021-05-06 18:39:09 -0700

    React-Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./containers/Home";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('app')
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  document.getElementById("app")
 );
