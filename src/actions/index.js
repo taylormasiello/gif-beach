0ac1bc201e5c5a27d881a03eea769a2ff61164a7
Taylor Masiello <masiellotay@gmail.com>
2021-05-07 10:51:23 -0700

    Adding Firebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,93 +1,106 @@
-import request from "superagent";
-import Firebase from "firebase";
+import request from 'superagent';
+import Firebase from 'firebase';
 
-export const OPEN_MODAL = "OPEN_MODAL";
-export const CLOSE_MODAL = "CLOSE_MODAL";
-export const REQUEST_GIFS = "REQUEST_GIFS";
-export const SIGN_OUT_USER = "SIGN_OUT_USER";
-export const SIGN_IN_USER = "SIGN_IN_USER";
-export const AUTH_ERROR = "AUTH_ERROR";
-export const AUTH_USER = "AUTH_USER";
+export const OPEN_MODAL = 'OPEN_MODAL';
+export const CLOSE_MODAL = 'CLOSE_MODAL';
+export const REQUEST_GIFS = 'REQUEST_GIFS';
+export const SIGN_OUT_USER = 'SIGN_OUT_USER';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const AUTH_USER = 'AUTH_USER';
 
-const API_URL = "http://api.giphy.com/v1/gifs/search?q=";
-const API_KEY = "&api_key=dc6zaTOxFJmzC";
+const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
+const API_KEY = '&api_key=dc6zaTOxFJmzC';
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
 };
 
 Firebase.initializeApp(config);
 
 export function requestGifs(term = null) {
-  return function (dispatch) {
-    request
-      .get(`${API_URL}${term.replace(/\s/g, "+")}${API_KEY}`)
-      .then((response) => {
-        dispatch({
-          type: REQUEST_GIFS,
-          payload: response,
+    return function(dispatch) {
+        request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`).then(response => {
+            dispatch({
+                type: REQUEST_GIFS,
+                payload: response
+            });
         });
-      });
-  };
+    }
 }
 
 export function openModal(gif) {
-  return {
-    type: OPEN_MODAL,
-    gif,
-  };
+    return {
+        type: OPEN_MODAL,
+        gif
+    }
 }
 
 export function closeModal() {
-  return {
-    type: CLOSE_MODAL,
-  };
+    return {
+        type: CLOSE_MODAL
+    }
 }
 
 export function signUpUser(credentials) {
-  return function (dispatch) {
-    Firebase.auth()
-      .createUserWithEmailAndPassword(credentials.email, credentials.password)
-      .then((response) => {
-        dispatch(authUser());
-      })
-      .catch((error) => {
-        console.log(error);
-        dispatch(authError(error));
-      });
-  };
+    return function(dispatch) {
+        Firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
+            .then(response => {
+                dispatch(authUser());
+            })
+            .catch(error => {
+                console.log(error);
+                dispatch(authError(error));
+            });
+    }
 }
 
 export function signInUser(credentials) {
-  return function (dispatch) {
-    Firebase.auth()
-      .signInWithEmailAndPassword(credentials.email, credentials.password)
-      .then((response) => {
-        dispatch(authUser());
-      })
-      .catch((error) => {
-        dispatch(authError(error));
-      });
-  };
+    return function(dispatch) {
+        Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
+            .then(response => {
+                dispatch(authUser());
+            })
+            .catch(error => {
+                dispatch(authError(error));
+            });
+    }
 }
 
 export function signOutUser() {
-  return {
-    type: SIGN_OUT_USER,
-  };
+    return function (dispatch) {
+        Firebase.auth().signOut()
+            .then(() =>{
+                dispatch({
+                    type: SIGN_OUT_USER
+                })
+            });
+    }
+}
+
+
+export function verifyAuth() {
+    return function (dispatch) {
+        Firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                dispatch(authUser());
+            } else {
+                dispatch(signOutUser());
+            }
+        });
+    }
 }
 
 export function authUser() {
-  return {
-    type: AUTH_USER,
-  };
+    return {
+        type: AUTH_USER
+    }
 }
 
 export function authError(error) {
-  return {
-    type: AUTH_ERROR,
-    payload: error,
-  };
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    }
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,28 +1,31 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
-import rootReducer from "../reducers";
-import createHistory from "history/createBrowserHistory";
-import { routerMiddleware } from "react-router-redux";
+import { createStore, compose, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import rootReducer from '../reducers';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+import * as Actions from '../actions';
 
 export const history = createHistory();
 
 export function configureStore(initialState) {
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(reduxThunk, routerMiddleware(history)),
-      window.devToolsExtension ? window.devToolsExtension() : (f) => f
-    )
-  );
+    const store = createStore(
+        rootReducer,
+        initialState,
+        compose (
+            applyMiddleware(reduxThunk, routerMiddleware(history)),
+            window.devToolsExtension ? window.devToolsExtension() : f => f
+        )
+    );
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept("../reducers", () => {
-      const nextRootReducer = require("../reducers").default;
-      store.replaceReducer(nextRootReducer);
-    });
-  }
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers').default;
+            store.replaceReducer(nextRootReducer);
+        });
+    }
 
-  return store;
+    store.dispatch(Actions.verifyAuth());
+
+    return store;
 }
