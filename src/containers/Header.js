74391d2812861ff2144487bd0c1b0bfad403f844
Taylor Masiello <masiellotay@gmail.com>
2021-05-06 21:41:58 -0700

    Private and Public Routes

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,43 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import * as Actions from "../actions";
 
 class Header extends React.Component {
+  handleSignout() {
+    this.props.signOutUser();
+  }
+
+  renderAuthLinks() {
+    if (this.props.authenticated) {
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/favorites">
+            My Favorites
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <a className="nav-link" href="#" onClick={() => this.handleSignout()}>
+            Sign Out
+          </a>
+        </li>,
+      ];
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">
+            Sign Up
+          </Link>
+        </li>,
+      ];
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-default">
@@ -13,16 +48,7 @@ class Header extends React.Component {
             </Link>
           </div>
           <ul className="nav navbar-nav navbar-right">
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
-                Login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/signup">
-                Signup
-              </Link>
-            </li>
+            {this.renderAuthLinks()}
           </ul>
         </div>
       </nav>
@@ -31,7 +57,9 @@ class Header extends React.Component {
 }
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    authenticated: state.auth.authenticated,
+  };
 }
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, Actions)(Header);
