0a281d522bc54c645ed241c3c286e7fa39c41045
Taylor Masiello <masiellotay@gmail.com>
2021-05-07 13:53:10 -0700

    Ready

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import GifItem from './GifItem';
+import React from "react";
+import GifItem from "./GifItem";
 
 const GifList = (props) => {
-    const gifItems = props.gifs.map((image) => {
-        return <GifItem key={image.id}
-                        gif={image}
-                        onGifSelect={props.onGifSelect}
-                        onFavoriteSelect={props.onFavoriteSelect}
-                        onFavoriteDeselect={props.onFavoriteDeselect}
-                        isAuthenticated={props.isAuthenticated}
-                        isFavorite={props.isFavorite} />
-    });
-
+  const gifItems = props.gifs.map((image) => {
     return (
-        <div className="gif-list">{gifItems}</div>
+      <GifItem
+        key={image.id}
+        gif={image}
+        onGifSelect={props.onGifSelect}
+        onFavoriteSelect={props.onFavoriteSelect}
+        onFavoriteDeselect={props.onFavoriteDeselect}
+        isAuthenticated={props.isAuthenticated}
+        isFavorite={props.isFavorite}
+      />
     );
+  });
+
+  return <div className="gif-list">{gifItems}</div>;
 };
 
 export default GifList;
