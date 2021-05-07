ea5049bf72ec41ce9c39338a86c6e6a288180388
Taylor Masiello <masiellotay@gmail.com>
2021-05-07 10:37:50 -0700

    Before Firebase and Favorites

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,45 +1,93 @@
-import request from 'superagent';
+import request from "superagent";
+import Firebase from "firebase";
 
-export const OPEN_MODAL = 'OPEN_MODAL';
-export const CLOSE_MODAL = 'CLOSE_MODAL';
-export const REQUEST_GIFS = 'REQUEST_GIFS';
-export const SIGN_IN_USER = 'SIGN_IN_USER';
-export const SIGN_OUT_USER = 'SIGN_OUT_USER';
+export const OPEN_MODAL = "OPEN_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export const REQUEST_GIFS = "REQUEST_GIFS";
+export const SIGN_OUT_USER = "SIGN_OUT_USER";
+export const SIGN_IN_USER = "SIGN_IN_USER";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const AUTH_USER = "AUTH_USER";
 
-const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
-const API_KEY = '&api_key=dc6zaTOxFJmzC';
+const API_URL = "http://api.giphy.com/v1/gifs/search?q=";
+const API_KEY = "&api_key=dc6zaTOxFJmzC";
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+};
+
+Firebase.initializeApp(config);
 
 export function requestGifs(term = null) {
-    const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
-
-    return {
-        type: REQUEST_GIFS,
-        payload: data
-    }
+  return function (dispatch) {
+    request
+      .get(`${API_URL}${term.replace(/\s/g, "+")}${API_KEY}`)
+      .then((response) => {
+        dispatch({
+          type: REQUEST_GIFS,
+          payload: response,
+        });
+      });
+  };
 }
 
 export function openModal(gif) {
-    return {
-        type: OPEN_MODAL,
-        gif
-    }
+  return {
+    type: OPEN_MODAL,
+    gif,
+  };
 }
 
 export function closeModal() {
-    return {
-        type: CLOSE_MODAL
-    }
+  return {
+    type: CLOSE_MODAL,
+  };
 }
 
-export function signInUser() {
-    return {
-        type: SIGN_IN_USER
-    }
+export function signUpUser(credentials) {
+  return function (dispatch) {
+    Firebase.auth()
+      .createUserWithEmailAndPassword(credentials.email, credentials.password)
+      .then((response) => {
+        dispatch(authUser());
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authError(error));
+      });
+  };
 }
 
+export function signInUser(credentials) {
+  return function (dispatch) {
+    Firebase.auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then((response) => {
+        dispatch(authUser());
+      })
+      .catch((error) => {
+        dispatch(authError(error));
+      });
+  };
+}
 
 export function signOutUser() {
-    return {
-        type: SIGN_OUT_USER
-    }
+  return {
+    type: SIGN_OUT_USER,
+  };
+}
+
+export function authUser() {
+  return {
+    type: AUTH_USER,
+  };
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error,
+  };
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,28 +1,28 @@
-import { createStore, compose, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
-import rootReducer from '../reducers';
-import createHistory from 'history/createBrowserHistory';
-import { routerMiddleware } from 'react-router-redux';
+import { createStore, compose, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import rootReducer from "../reducers";
+import createHistory from "history/createBrowserHistory";
+import { routerMiddleware } from "react-router-redux";
 
 export const history = createHistory();
 
 export function configureStore(initialState) {
-    const store = createStore(
-        rootReducer,
-        initialState,
-        compose (
-            applyMiddleware(ReduxPromise, routerMiddleware(history)),
-            window.devToolsExtension ? window.devToolsExtension() : f => f
-        )
-    );
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(reduxThunk, routerMiddleware(history)),
+      window.devToolsExtension ? window.devToolsExtension() : (f) => f
+    )
+  );
 
-    if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept('../reducers', () => {
-            const nextRootReducer = require('../reducers').default;
-            store.replaceReducer(nextRootReducer);
-        });
-    }
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept("../reducers", () => {
+      const nextRootReducer = require("../reducers").default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
 
-    return store;
+  return store;
 }
