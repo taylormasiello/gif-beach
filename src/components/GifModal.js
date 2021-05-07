864ca9a6f890ef10cacbae24cf8b9a51ce22dcbb
Taylor Masiello <masiellotay@gmail.com>
2021-05-07 13:28:18 -0700

    Formatting

--- src/components/GifModal.js
+++ src/components/GifModal.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
 const GifModal = (props) => {
-    if (!props.selectedGif) {
-        return <div></div>;
-    }
+  if (!props.selectedGif) {
+    return <div></div>;
+  }
 
-    return (
-        <Modal
-            isOpen={ props.modalIsOpen }
-            onRequestClose={ () => props.onRequestClose() }>
-            <div className="gif-modal">
-                <img src={ props.selectedGif.images.original.url } alt="" />
-                <p><strong>Source:</strong> <a href={ props.selectedGif.source }>{ props.selectedGif.source }</a></p>
-                <p><strong>Rating:</strong> { props.selectedGif.rating }</p>
+  return (
+    <Modal
+      isOpen={props.modalIsOpen}
+      onRequestClose={() => props.onRequestClose()}
+    >
+      <div className="gif-modal">
+        <img src={props.selectedGif.images.original.url} alt="" />
+        <h3>
+          <strong>Name:</strong> {props.selectedGif.title}
+        </h3>
+        <h4>
+          <strong>Rating:</strong> {props.selectedGif.rating}
+        </h4>
+        <p>
+          <strong>Source:</strong>{" "}
+          <a href={props.selectedGif.source}>{props.selectedGif.source}</a>
+        </p>
 
-                <button onClick={() => props.onRequestClose()}>close</button>
-            </div>
-        </Modal>
-    );
+        <button onClick={() => props.onRequestClose()}>close</button>
+      </div>
+    </Modal>
+  );
 };
 
 export default GifModal;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
-    onInputChange(term) {
-        this.props.onTermChange(term);
-    }
+  onInputChange(term) {
+    this.props.onTermChange(term);
+  }
 
-    render() {
-        return (
-            <div className="search">
-                <input placeholder="Enter text to search for gifs!" onChange={event => this.onInputChange(event.target.value)} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="search">
+        <input
+          placeholder="What gifs will wash up on shore?"
+          onChange={(event) => this.onInputChange(event.target.value)}
+        />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
