1df365c81c8407f8a53e71ad362173ff6dd6e619
Taylor Masiello <masiellotay@gmail.com>
2021-05-07 11:35:57 -0700

    Config

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,136 +1,149 @@
-import request from 'superagent';
-import Firebase from 'firebase';
+import request from "superagent";
+import Firebase from "firebase";
 
-export const OPEN_MODAL = 'OPEN_MODAL';
-export const CLOSE_MODAL = 'CLOSE_MODAL';
-export const REQUEST_GIFS = 'REQUEST_GIFS';
-export const FETCH_FAVORITED_GIFS = 'FETCH_FAVORITED_GIFS';
-export const SIGN_OUT_USER = 'SIGN_OUT_USER';
-export const AUTH_ERROR = 'AUTH_ERROR';
-export const AUTH_USER = 'AUTH_USER';
+export const OPEN_MODAL = "OPEN_MODAL";
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export const REQUEST_GIFS = "REQUEST_GIFS";
+export const FETCH_FAVORITED_GIFS = "FETCH_FAVORITED_GIFS";
+export const SIGN_OUT_USER = "SIGN_OUT_USER";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const AUTH_USER = "AUTH_USER";
 
-const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
-const API_KEY = '&api_key=dc6zaTOxFJmzC';
+const API_URL = "http://api.giphy.com/v1/gifs/search?q=";
+const API_KEY = "&api_key=dc6zaTOxFJmzC";
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
+  apiKey: "AIzaSyDQFcCe810d5ej9xm6CaPQ63Vqgb84wygo",
+  authDomain: "gif-test-9b585.firebaseapp.com",
+  projectId: "gif-test-9b585",
+  storageBucket: "gif-test-9b585.appspot.com",
+  messagingSenderId: "970283311526",
+  appId: "1:970283311526:web:2d9b0363b6df4bae94ef67",
+  measurementId: "G-6X9GKQ0V41",
 };
 
 Firebase.initializeApp(config);
 
 export function requestGifs(term = null) {
-    return function(dispatch) {
-        request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`).then(response => {
-            dispatch({
-                type: REQUEST_GIFS,
-                payload: response
-            });
+  return function (dispatch) {
+    request
+      .get(`${API_URL}${term.replace(/\s/g, "+")}${API_KEY}`)
+      .then((response) => {
+        dispatch({
+          type: REQUEST_GIFS,
+          payload: response,
         });
-    }
+      });
+  };
 }
 
-export function favoriteGif({selectedGif}) {
-    const userUid = Firebase.auth().currentUser.uid;
-    const gifId = selectedGif.id;
+export function favoriteGif({ selectedGif }) {
+  const userUid = Firebase.auth().currentUser.uid;
+  const gifId = selectedGif.id;
 
-    return dispatch => Firebase.database().ref(userUid).update({
-        [gifId]: selectedGif
-    });
+  return (dispatch) =>
+    Firebase.database()
+      .ref(userUid)
+      .update({
+        [gifId]: selectedGif,
+      });
 }
 
-export function unfavoriteGif({selectedGif}) {
-    const userUid = Firebase.auth().currentUser.uid;
-    const gifId = selectedGif.id;
+export function unfavoriteGif({ selectedGif }) {
+  const userUid = Firebase.auth().currentUser.uid;
+  const gifId = selectedGif.id;
 
-    return dispatch => Firebase.database().ref(userUid).child(gifId).remove();
+  return (dispatch) => Firebase.database().ref(userUid).child(gifId).remove();
 }
 
 export function fetchFavoritedGifs() {
-    return function(dispatch) {
-        const userUid = Firebase.auth().currentUser.uid;
+  return function (dispatch) {
+    const userUid = Firebase.auth().currentUser.uid;
 
-        Firebase.database().ref(userUid).on('value', snapshot => {
-            dispatch({
-                type: FETCH_FAVORITED_GIFS,
-                payload: snapshot.val()
-            })
+    Firebase.database()
+      .ref(userUid)
+      .on("value", (snapshot) => {
+        dispatch({
+          type: FETCH_FAVORITED_GIFS,
+          payload: snapshot.val(),
         });
-    }
+      });
+  };
 }
 
 export function openModal(gif) {
-    return {
-        type: OPEN_MODAL,
-        gif
-    }
+  return {
+    type: OPEN_MODAL,
+    gif,
+  };
 }
 
 export function closeModal() {
-    return {
-        type: CLOSE_MODAL
-    }
+  return {
+    type: CLOSE_MODAL,
+  };
 }
 
 export function signUpUser(credentials) {
-    return function(dispatch) {
-        Firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
-            .then(response => {
-                dispatch(authUser());
-            })
-            .catch(error => {
-                console.log(error);
-                dispatch(authError(error));
-            });
-    }
+  return function (dispatch) {
+    Firebase.auth()
+      .createUserWithEmailAndPassword(credentials.email, credentials.password)
+      .then((response) => {
+        dispatch(authUser());
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(authError(error));
+      });
+  };
 }
 
 export function signInUser(credentials) {
-    return function(dispatch) {
-        Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
-            .then(response => {
-                dispatch(authUser());
-            })
-            .catch(error => {
-                dispatch(authError(error));
-            });
-    }
+  return function (dispatch) {
+    Firebase.auth()
+      .signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then((response) => {
+        dispatch(authUser());
+      })
+      .catch((error) => {
+        dispatch(authError(error));
+      });
+  };
 }
 
 export function signOutUser() {
-    return function (dispatch) {
-        Firebase.auth().signOut()
-            .then(() =>{
-                dispatch({
-                    type: SIGN_OUT_USER
-                })
-            });
-    }
+  return function (dispatch) {
+    Firebase.auth()
+      .signOut()
+      .then(() => {
+        dispatch({
+          type: SIGN_OUT_USER,
+        });
+      });
+  };
 }
 
-
 export function verifyAuth() {
-    return function (dispatch) {
-        Firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                dispatch(authUser());
-            } else {
-                dispatch(signOutUser());
-            }
-        });
-    }
+  return function (dispatch) {
+    Firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(authUser());
+      } else {
+        dispatch(signOutUser());
+      }
+    });
+  };
 }
 
 export function authUser() {
-    return {
-        type: AUTH_USER
-    }
+  return {
+    type: AUTH_USER,
+  };
 }
 
 export function authError(error) {
-    return {
-        type: AUTH_ERROR,
-        payload: error
-    }
+  return {
+    type: AUTH_ERROR,
+    payload: error,
+  };
 }
